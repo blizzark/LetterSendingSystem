bd044e86f9ef78f58cbaf979d30d8924bf850982
blizzark <blizzark@ya.ru>
2023-11-09 22:00:47 +0300

    Добавьте файлы проекта.

---
+++ LetterSendingSystem/MailForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LetterSendingSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для MailForm.xaml
+    /// </summary>
+    public partial class MailForm : Window
+    {
+        public MailForm()
+        {
+            InitializeComponent();
+            ComboNames.ItemsSource = GetCountries();
+        }
+        private List<string> GetCountries()
+        {
+            List<string> countries = new List<string>();
+            countries.Add("Россия");
+            countries.Add("Украина");
+            countries.Add("Беларусь");
+            countries.Add("Казахстан");
+            countries.Add("Армения");
+            countries.Add("Грузия");
+            // и так далее...
+            return countries;
+        }
+        private List<string> GetFilteredCountries(string searchText)
+        {
+            List<string> filteredCountries = new List<string>();
+            foreach (string country in GetCountries())
+            {
+                if (country.StartsWith(searchText, StringComparison.OrdinalIgnoreCase))
+                {
+                    filteredCountries.Add(country);
+                }
+            }
+            return filteredCountries;
+        }
+
+        private void ComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = ComboNames.Text;
+            ComboNames.ItemsSource = GetFilteredCountries(searchText);
+        }
+    }
+}
